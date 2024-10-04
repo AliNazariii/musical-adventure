b41fe4f9fc57fbed36a7e374116131a38c41c330
Ali Nazari <ali4nazari4@gmail.com>
2024-10-04 19:08:39 +0330

    Add 02-unique-ids

---
+++ 02-unique-ids/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/google/uuid"
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "generate_ok"
+		body["id"] = uuid.New().String()
+
+		return n.Reply(msg, body)
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
