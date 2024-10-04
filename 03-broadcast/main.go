aeaca6ae8fabe99fe7db1634cab87ea07802359c
Ali Nazari <ali4nazari4@gmail.com>
2024-10-04 19:24:19 +0330

    Add 03-broadcast

---
+++ 03-broadcast/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"sync"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+
+	messages := make([]float64, 0)
+	var lock sync.Mutex
+
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "broadcast_ok"
+
+		lock.Lock()
+		messages = append(messages, body["message"].(float64))
+		lock.Unlock()
+
+		delete(body, "message")
+
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("read", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "read_ok"
+
+		lock.Lock()
+		body["messages"] = messages
+		lock.Unlock()
+
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "topology_ok"
+
+		delete(body, "topology")
+
+		return n.Reply(msg, body)
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
